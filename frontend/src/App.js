223d3acda789531831976b0551d130de36ee18ec
ansuljain789 <ansuljain789@gmail.com>
2025-07-24 23:59:27 +0530

    app.js changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,23 @@
-import { Route, Routes } from 'react-router-dom';
+
+import { Route ,Routes} from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/HomePage';
-import ChatPage from './pages/ChatPage';
+import ChatPage from './pages/ChatPage'
 import ForgotPassword from './components/Authentication/ForgotPassword';
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/chats' element={<ChatPage />} />
-        <Route path='/forgot' element={<ForgotPassword />} />
-      </Routes>
-    </div>
+   <Routes>
+    <Route path='/' Component={HomePage} />
+    <Route path='/chats' Component={ChatPage} />
+    <Route path="/forgot" element={<ForgotPassword />} />
+
+  
+    </Routes>
+    
+
+     </div>
   );
 }
 
